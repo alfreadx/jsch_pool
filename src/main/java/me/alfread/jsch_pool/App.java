2f8fe89d51fb1881eb1446cd3c3b43c5aec2ca9c
alfreadx <alfreadx@gmail.com>
2016-09-01 23:22:42 +0800

    first commit

---
+++ src/main/java/me/alfread/jsch_pool/App.java
@@ -0,0 +1,19 @@
+package me.alfread.jsch_pool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+	
+	
+	public static void main(String[] args) {
+		log.info("Hello world");
+		System.out.println("Hello World!");
+	}
+}
