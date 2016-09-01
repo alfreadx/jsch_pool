93f35ec5437ab1864efb27a38d475e96507e41ce
alfreadx <alfreadx@gmail.com>
2016-09-02 01:05:41 +0800

    新增sshmanager,初始的可連線的程式

---
+++ src/main/java/me/alfread/ssh/impl/SSHManager.java
@@ -0,0 +1,101 @@
+package me.alfread.ssh.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class SSHManager {
+
+	private static final Logger log = LoggerFactory.getLogger(SSHManager.class);
+
+	private JSch jschSSHChannel;
+	private String strUserName;
+	private String strConnectionIP;
+	private int intConnectionPort;
+	private String strPassword;
+	private Session sesConnection;
+	private int intTimeOut;
+
+	private void doCommonConstructorActions(String userName, String password, String connectionIP,
+			String knownHostsFileName) {
+		jschSSHChannel = new JSch();
+		try {
+			jschSSHChannel.setKnownHosts(knownHostsFileName);
+		} catch (JSchException jschX) {
+			log.error("{}",jschX.getMessage());
+		}
+		strUserName = userName;
+		strPassword = password;
+		strConnectionIP = connectionIP;
+	}
+
+	public SSHManager(String userName, String password, String connectionIP, String knownHostsFileName) {
+		doCommonConstructorActions(userName, password, connectionIP, knownHostsFileName);
+		intConnectionPort = 22;
+		intTimeOut = 60000;
+	}
+
+	public SSHManager(String userName, String password, String connectionIP, String knownHostsFileName,
+			int connectionPort) {
+		doCommonConstructorActions(userName, password, connectionIP, knownHostsFileName);
+		intConnectionPort = connectionPort;
+		intTimeOut = 60000;
+	}
+
+	public SSHManager(String userName, String password, String connectionIP, String knownHostsFileName,
+			int connectionPort, int timeOutMilliseconds) {
+		doCommonConstructorActions(userName, password, connectionIP, knownHostsFileName);
+		intConnectionPort = connectionPort;
+		intTimeOut = timeOutMilliseconds;
+	}
+
+	public String connect() {
+		String errorMessage = null;
+		try {
+			sesConnection = jschSSHChannel.getSession(strUserName, strConnectionIP, intConnectionPort);
+			sesConnection.setPassword(strPassword);
+			// UNCOMMENT THIS FOR TESTING PURPOSES, BUT DO NOT USE IN PRODUCTION
+			// sesConnection.setConfig("StrictHostKeyChecking", "no");
+			sesConnection.connect(intTimeOut);
+		} catch (JSchException jschX) {
+			errorMessage = jschX.getMessage();
+		}
+		return errorMessage;
+	}
+
+	public String sendCommand(String command) {
+		StringBuilder outputBuffer = new StringBuilder();
+		try {
+			Channel channel = sesConnection.openChannel("exec");
+			((ChannelExec) channel).setCommand(command);
+			InputStream commandOutput = channel.getInputStream();
+			channel.connect();
+			int readByte = commandOutput.read();
+			while (readByte != 0xffffffff) {
+				outputBuffer.append((char) readByte);
+				readByte = commandOutput.read();
+			}
+			channel.disconnect();
+		} catch (IOException ioX) {
+			log.warn("{}",ioX.getMessage());
+			return null;
+		} catch (JSchException jschX) {
+			log.warn("{}",jschX.getMessage());
+			return null;
+		}
+		return outputBuffer.toString();
+	}
+
+	public void close() {
+		sesConnection.disconnect();
+	}
+
+}
