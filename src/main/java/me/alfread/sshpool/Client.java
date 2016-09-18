5cfe71462e194ed5dcf6e70c59939418efca9ac7
alfreadx <alfreadx@gmail.com>
2016-09-18 20:42:55 +0800

    修改客戶端以測試連線池功能； 修改SSHUtils.sshExecute()及getSessionId()以測試連線池功能。 修改連線池的設定

--- src/main/java/me/alfread/sshpool/Client.java
+++ src/main/java/me/alfread/sshpool/Client.java
@@ -1,14 +1,82 @@
 package me.alfread.sshpool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Client {
+	
+	private static final Logger log = LoggerFactory.getLogger(Client.class);
 	
 	public static void main(String[] args) {
 		
-		String result = SSHUtils.sendCommand("cmd /c dir");
+		final Client client = new Client();
 		
-		System.out.println("result: " + result);
+		final int loopCount = 10;
+		int threadCount = 10;
 		
+		List<Thread> threads = new ArrayList<Thread>();
+		
+		for(int i=0;i < threadCount;i ++){
+			threads.add(new Thread(client.getWorker(loopCount)));
+		}
+		
+		log.info("Thread add done.");
+		
+		try {
+			log.info("start");
+			for(Thread t : threads){
+				t.start();
+				Thread.sleep(1000);
+			}
+			
+		} catch (Exception e) {
+			log.error("start thread error",e);
+		}
 	}
 	
+	
+	public void runTest(){
+		String result = "";
+//		result = SSHUtils.sshexecute("cmd /c dir D:\\mock_fs");
+//		result = SSHUtils.sshexecute("ls /");
+		result = SSHUtils.getSessionId();
+		log.info("result:{}" , result);
+	}
+	
+	public Runnable getWorker(final int loopCount){
+		Runnable worker =  new Runnable() {
+			
+			public void run() {
+				for(int i=0;i<loopCount;i++)
+					runTest();
+			}
+		};
+		return worker;
+	}
 
+	public void runWorker(final int loopCount){
+		getWorker(loopCount).run();
+	}
+	
+	
+	public Callable<Long> getWorker2(final int loopCount){
+		Callable<Long> c = new Callable<Long>() {
+			
+			public Long call() throws Exception {
+				long start = System.currentTimeMillis();
+				for(int i=0;i<loopCount;i++){
+					runTest();
+				}
+				long end = System.currentTimeMillis();
+				return end - start;
+			}
+		};
+		
+		return c;
+	}
+	
 }
--- src/main/java/me/alfread/sshpool/SSHUtils.java
+++ src/main/java/me/alfread/sshpool/SSHUtils.java
@@ -2,6 +2,7 @@ package me.alfread.sshpool;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.slf4j.Logger;
@@ -18,18 +19,26 @@ public class SSHUtils {
 	
 	private static ConnectionPoolFactory connPoolFactory ;
 	
+	private static Random rand = null;
+	
 	static {
 		init();
+		initRandom();
 	}
 	
 	private static void init(){
 		GenericObjectPoolConfig config = new GenericObjectPoolConfig();
-		config.setMaxTotal(10);
+		config.setMaxTotal(15);
+		config.setMaxWaitMillis(1000);
+		config.setMinIdle(3);
 		connPoolFactory = 
 				new ConnectionPoolFactory(config, 
-						"mes", "a123456", "127.0.0.1", 22, 1800);
+						"mes", "P@ssw0rd", "10.1.21.197", 22, 1800);
 	}
 	
+	private static void initRandom(){
+		rand = new Random(System.currentTimeMillis());
+	}
 	
 	public static String sendCommand(String command){
 		StringBuilder outputBuffer = new StringBuilder();
@@ -64,4 +73,67 @@ public class SSHUtils {
 		return outputBuffer.toString();
 	}
 
+	
+	public static String sshexecute(String command){
+		StringBuilder outputBuffer = new StringBuilder();
+		Session session = null;
+		try {
+			session = connPoolFactory.getSession();
+			Channel channel = session.openChannel("exec");
+			((ChannelExec) channel).setCommand(command);
+			InputStream commandOutput = channel.getInputStream();
+			channel.connect();
+			int readByte = commandOutput.read();
+			while (readByte != 0xffffffff) {
+				outputBuffer.append((char) readByte);
+				readByte = commandOutput.read();
+			}
+			outputBuffer.append("\n\n\n\n session id:").append(session.toString());
+			
+			Thread.sleep(3000);
+			channel.disconnect();
+			
+		} catch (IOException ioX) {
+			log.warn(ioX.getMessage());
+			return null;
+		} catch (JSchException jschX) {
+			log.warn(jschX.getMessage());
+			return null;
+		} catch (Exception e) {
+			log.warn(e.getMessage());
+			return null;
+		} finally{
+			if(session != null){
+				connPoolFactory.releaseSession(session);
+			}
+		}
+		return outputBuffer.toString();
+	}
+	
+	public static String getSessionId(){
+		Session session = null;
+		String sid = "";
+		try {
+			session = connPoolFactory.getSession();
+			sid = session.toString();
+			long sleepMs = getSleepMillis();
+			log.info("sleep {},sid:{}",sleepMs,sid);
+			Thread.sleep(sleepMs);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			if(session != null){
+				connPoolFactory.releaseSession(session);
+			}
+		}
+		
+		
+		return sid;
+	}
+	
+	
+	private static long getSleepMillis(){
+		return ( rand.nextInt(10) + 1 ) * 1000;
+	}
 }
